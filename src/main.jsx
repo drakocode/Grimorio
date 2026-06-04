fef33025bacc3d0034936a4d9ddd9551ac1cf292
drakocode <erikkayser843@gmail.com>
2026-06-03 21:39:32 -0300

    teste1

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app.jsx"
-import "./Styles/globals.css";
+import "./Styles/Globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
