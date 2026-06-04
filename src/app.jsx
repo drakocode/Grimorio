85bac5e7ee8ff2cb84e40a1927faea023c3725c0
drakocode <erikkayser843@gmail.com>
2026-06-03 21:50:43 -0300

    teste5

--- src/app.jsx
+++ src/app.jsx
@@ -14,7 +14,7 @@ import TarotDetailsTemplate from "./pages/Tarot/TarotDetailsTemplate";
 
 /* import DynamicDetails from './pages/DynamicDetails'; */
 import SignDetailsTemplate from "./pages/Astrology/Signs/SignDetailsTemplate";
-import "./styles/App.css";
+import "./styles/app.css";
  
 function App() {
   return (
