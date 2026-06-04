4398c6e79c74ead737afee2b2642c4ffe759abf2
drakocode <erikkayser843@gmail.com>
2026-06-03 22:09:49 -0300

    fix: alinhamento molecular de maiúsculas e minúsculas na raiz

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from "./app.jsx"; // <-- Alterado de './App.jsx' para './app.jsx'
+import App from "./App.jsx"; 
 import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
