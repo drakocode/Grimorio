b044de352aabc27b53e68ae37cef65dd21b1055a
drakocode <erikkayser843@gmail.com>
2026-06-03 22:01:14 -0300

    fix: alinhamento estrito de maiúsculas nos arquivos raiz

--- src/app.jsx
+++ src/app.jsx
@@ -14,7 +14,7 @@ import TarotDetailsTemplate from "./pages/Tarot/TarotDetailsTemplate";
 
 /* import DynamicDetails from './pages/DynamicDetails'; */
 import SignDetailsTemplate from "./pages/Astrology/Signs/SignDetailsTemplate";
-import "./Styles/App.css";
+import "./Styles/app.css";
  
 function App() {
   return (
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx"
-import "./Styles/Globals.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from "./app.jsx"; // <-- Alterado de './App.jsx' para './app.jsx'
+import "./styles/globals.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
